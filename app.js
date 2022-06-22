d6c132e65b86c8e4a507e41b3d1d34b423e2ee30
zohirulislam1 <codershahin84@gmail.com>
2022-06-22 23:35:51 +0600

    addEvenlisteniner

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log('I am in a separate file')
+function inputValue(isIncreasing) {
+    const phoneInput = document.getElementById("phone_input");
+    let phoneValue = phoneInput.value;
+
+    if(isIncreasing){
+        phoneValue = parseInt(phoneValue) + 1;
+    }else if(phoneValue > 1){
+        phoneValue = parseInt(phoneValue) - 1;
+    }
+    phoneInput.value = phoneValue;
+
+    const phonePrice = document.getElementById("phone_price");
+     phonePrice.innerText = phoneValue * 1200;
+  
+   
+}
+
+document.getElementById("phone_plus_btn").addEventListener("click" ,function(){
+    // console.log("shahin")
+    // const phoneInput = document.getElementById("phone_input");
+    // const phoneValue = phoneInput.value;
+    // phoneInput.value = parseInt(phoneValue) + 1;
+    inputValue(true)
+})
+
+document.getElementById('phone_minus_btn').addEventListener("click" ,function(){
+//   const phoneInput = document.getElementById("phone_input")
+//   const phoneValue = phoneInput.value;
+//   phoneInput.value = parseInt(phoneValue) - 1;
+inputValue(false)
+})
